cb6a12025052390aa5dd196f00e8b2739df93b6f
Elan Ruusamäe <glen@delfi.ee>
2016-02-02 12:16:00 +0000

    channels export script from lauri
    Changed files:
        export-slack-channels.php -> 1.1

---
+++ export-slack-channels.php
@@ -0,0 +1,35 @@
+<?php
+$tok = getenv('slack_api_token');
+$url = "https://slack.com/api/channels.list?token=$tok";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+$res = curl_exec($ch);
+curl_close($ch);
+if (!$res){
+	echo 'No response!' . PHP_EOL;
+	exit(1);
+}
+$data = json_decode($res);
+if(!$data->ok || $data->ok != true) {
+	echo 'Response from API NOT OK:' . PHP_EOL;
+	var_dump($res);
+	exit(1);
+}
+if(!$data->channels || !count($data->channels)){
+	echo 'No channels in response!' . PHP_EOL;
+	var_dump($res);
+	exit(1);
+}
+
+$fmt = ('|%-90s|%-150s|%-52s|' . PHP_EOL);
+printf($fmt, 'Channel Name', 'Channel Purpose', 'Channel Topic');
+foreach($data->channels as $chan) {
+	printf($fmt,
+	'[[https://team.slack.com/messages/' . $chan->name . '|#' . $chan->name . ']]',
+	$chan->purpose->value,
+	$chan->topic->value);
+}
