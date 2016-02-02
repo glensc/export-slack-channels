a75f49e17f628c0b5b28adb615342d95154768b8
Elan Ruusamäe <glen@delfi.ee>
2016-02-02 13:22:36 +0000

    commit page to dokuwiki
    Changed files:
        export-slack-channels.php -> 1.5

--- export-slack-channels.php
+++ export-slack-channels.php
@@ -2,6 +2,8 @@
 <?php
 $tok = 'TOKEN';
 $url = "https://slack.com/api/channels.list?token=$tok";
+$dwpage = '/usr/share/dokuwiki/bin/dwpage.php';
+$page = 'it:slack_channels';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -24,11 +26,24 @@ if(!$data->channels || !count($data->channels)){
 	exit(1);
 }
 
+ob_start();
 $fmt = ('|%-90s|%-150s|%-52s|' . PHP_EOL);
-printf($fmt, 'Channel Name', 'Channel Purpose', 'Channel Topic');
+$header = sprintf($fmt, 'Channel Name', 'Channel Purpose', 'Channel Topic');
+$header = str_replace("|", "^", $header);
+echo $header;
+
 foreach($data->channels as $chan) {
 	printf($fmt,
 	'[[https://team.slack.com/messages/' . $chan->name . '|#' . $chan->name . ']]',
 	$chan->purpose->value,
 	$chan->topic->value);
 }
+$contents = ob_get_clean();
+
+# commit to wiki
+$tmpfile = tempnam(sys_get_temp_dir(), 'sl2dw');
+file_put_contents($tmpfile, $contents);
+
+$message = "updated slack channels info";
+exec("{$dwpage} commit -m '{$message}' -t {$tmpfile} {$page} 2>&1 | grep -v ^S:");
+unlink($tmpfile);
